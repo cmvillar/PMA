85910ed4ca5c9be1baa07d47a3122f62de39678e
cmvillar <cesarvillarcar@yahoo.es>
2013-04-03 23:23:53 +0200

    Primero hago que falle

---
+++ validator/src/com/pma/validator/ValidatorPwdSpec.java
@@ -0,0 +1,24 @@
+package com.pma.validator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Esta clase define las especificaciones que debe cumplir el validador
+ * 1.- tendr‡ que tener al menos un nœmero y una letra
+ * 2.- tendr‡ que tener al menos alguna mayœscula
+ * 3.- tendr‡ que tener una longitud mayor o igual a 4 caracteres
+ * 4.- tendr‡ que tener al menos una minœscula
+ * @author cmvillar
+ *
+ */
+
+public class ValidatorPwdSpec {
+
+	@Test
+	public void test() {
+		fail("");
+	}
+
+}
