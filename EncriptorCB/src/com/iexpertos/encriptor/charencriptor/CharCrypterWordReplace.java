0a70cc19c22e53a633734f03c8f17b46b90668ce
cmvillar <cesarvillarcar@yahoo.es>
2013-04-20 17:00:31 +0200

    Refatorización: Rename y simplificación bucle

--- EncriptorCB/src/com/iexpertos/encriptor/charencriptor/CharCrypterWordReplace.java
+++ EncriptorCB/src/com/iexpertos/encriptor/charencriptor/CharCrypterWordReplace.java
@@ -2,23 +2,22 @@ package com.iexpertos.encriptor.charencriptor;
 
 public class CharCrypterWordReplace implements CharCrypter {
 
-	private char[] replacement;
-	
-	public CharCrypterWordReplace(String charsToReplace){
-		this.replacement = charsToReplace.toCharArray();
+	private char[] charsToReplace;
+
+	public void setCharsToReplace(String charsToReplace){
+		this.charsToReplace = charsToReplace.toCharArray();
 	}
 	
 	@Override
 	public String crypt(int charValue) {
 		char result = (char)charValue;
-		 
-		for (int j = 0; j < replacement.length; j++)
-		{
-			if (replacement[j] == result)
+		for(char replaceChar: charsToReplace){
+			if (replaceChar == result)
 			{
 				result = (char)( charValue + 2);		
 			}
 		}
+		 
 		return String.valueOf(result);
 	}
 }
