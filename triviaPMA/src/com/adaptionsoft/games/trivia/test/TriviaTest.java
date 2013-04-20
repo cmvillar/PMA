fe1f7bab1189ffeb42f5a5b845ceee9ca63f2a73
cmvillar <cesarvillarcar@yahoo.es>
2013-04-20 18:55:16 +0200

    Hago que pasen los Tests de TriviaTest

--- triviaPMA/src/com/adaptionsoft/games/trivia/test/TriviaTest.java
+++ triviaPMA/src/com/adaptionsoft/games/trivia/test/TriviaTest.java
@@ -1,11 +1,8 @@
 package com.adaptionsoft.games.trivia.test;
 
-import java.io.IOException;
-import java.io.OutputStream;
 import java.io.PrintStream;
 import java.util.Random;
-import java.util.zip.CRC32;
-import java.util.zip.Checksum;
+
 
 import junit.framework.Assert;
 
@@ -27,8 +24,9 @@ public class TriviaTest extends AbstractTriviaTest{
 	public void outputStream() {
 		System.setOut(new PrintStream(checker));
 		Random rand = new Random(0L);
-		Game game = GameRunner.initialize();
-		GameRunner.run(game, rand);
+		GameRunner gameRunner = new GameRunner();
+		Game game = gameRunner.initialize();
+		gameRunner.run(game, rand);
 		Assert.assertEquals(1763398543L, checker.checksum.getValue());
 		
 	}
