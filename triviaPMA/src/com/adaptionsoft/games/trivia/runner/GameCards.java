b726cc22f11d535384320d347438ece9a600ed0c
cmvillar <cesarvillarcar@yahoo.es>
2013-04-20 18:22:23 +0200

    Refactor: meto lógica de negocio para askQuestion Creo constantes para determinar el tipo de pregunta a sacar

--- triviaPMA/src/com/adaptionsoft/games/trivia/runner/GameCards.java
+++ triviaPMA/src/com/adaptionsoft/games/trivia/runner/GameCards.java
@@ -3,10 +3,15 @@ package com.adaptionsoft.games.trivia.runner;
 import java.util.LinkedList;
 
 public class GameCards{
-    public LinkedList popQuestions = new LinkedList();
-    public LinkedList scienceQuestions = new LinkedList();
-    public LinkedList sportsQuestions = new LinkedList();
-    public LinkedList rockQuestions = new LinkedList();
+	public static int POP_CATEGORY = 0;
+	public static int SCIENCE_CATEGORY = 1;
+	public static int SPORT_CATEGORY = 2;
+	public static int ROCK_CATEGORY = 3;
+	
+    private LinkedList<String> popQuestions = new LinkedList<String>();
+    private LinkedList<String> scienceQuestions = new LinkedList<String>();
+    private LinkedList<String> sportsQuestions = new LinkedList<String>();
+    private LinkedList<String> rockQuestions = new LinkedList<String>();
     
     public GameCards(){
     	
@@ -20,4 +25,15 @@ public class GameCards{
 			rockQuestions.addLast("Rock Question " + i);
     	}
     }
+    
+	public void askQuestion(int categoryCard) {
+		if (categoryCard == POP_CATEGORY)
+			System.out.println(popQuestions.removeFirst());
+		if (categoryCard == SCIENCE_CATEGORY)
+			System.out.println(scienceQuestions.removeFirst());
+		if (categoryCard == SPORT_CATEGORY)
+			System.out.println(sportsQuestions.removeFirst());
+		if (categoryCard == ROCK_CATEGORY)
+			System.out.println(rockQuestions.removeFirst());		
+	}
 }
