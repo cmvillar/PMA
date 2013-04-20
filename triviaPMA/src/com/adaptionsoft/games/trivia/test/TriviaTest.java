2d8546ef960650feabd7e583a5d7abed3afa097f
cmvillar <cesarvillarcar@yahoo.es>
2013-04-20 17:42:33 +0200

    Hago pasar el Test

--- triviaPMA/src/com/adaptionsoft/games/trivia/test/TriviaTest.java
+++ triviaPMA/src/com/adaptionsoft/games/trivia/test/TriviaTest.java
@@ -1,16 +1,38 @@
 package com.adaptionsoft.games.trivia.test;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.Random;
+import java.util.zip.CRC32;
+import java.util.zip.Checksum;
+
 import junit.framework.Assert;
 
 import org.junit.Test;
 
+import com.adaptionsoft.games.trivia.runner.GameRunner;
+import com.adaptionsoft.games.uglytrivia.Game;
+
 
 public class TriviaTest {
 
+	class Checker extends OutputStream{
+		Checksum checksum = new CRC32();
+		public void write (int b) throws IOException{
+			checksum.update(b);
+		}
+	}
 	
 	@Test
 	public void outputStream() {
-		Assert.assertFalse(true);
+		Checker checker = new Checker();
+		System.setOut(new PrintStream(checker));
+		Random rand = new Random(0L);
+		Game game = GameRunner.initialize();
+		GameRunner.run(game, rand);
+		Assert.assertEquals(1763398543L, checker.checksum.getValue());
+		
 	}
 
 }
