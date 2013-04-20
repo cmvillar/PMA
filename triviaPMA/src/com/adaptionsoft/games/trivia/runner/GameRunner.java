42be1cacc50d3207b5e04f6bdf359bffaa1c4a4e
cmvillar <cesarvillarcar@yahoo.es>
2013-04-20 20:49:13 +0200

    Falla el test

--- triviaPMA/src/com/adaptionsoft/games/trivia/runner/GameRunner.java
+++ triviaPMA/src/com/adaptionsoft/games/trivia/runner/GameRunner.java
@@ -3,12 +3,13 @@ package com.adaptionsoft.games.trivia.runner;
 import java.util.Random;
 
 import com.adaptionsoft.games.trivia.dice.SingleDice;
+import com.adaptionsoft.games.trivia.dice.TenSidedDice;
 import com.adaptionsoft.games.uglytrivia.Game;
 
 
 public class GameRunner {
 
-	private static boolean notAWinner;
+	private static boolean isAWinner;
 
 	public static void  main(String[] args) {
 		Random rand = new Random();
@@ -19,28 +20,18 @@ public class GameRunner {
 	}
 	
 	public static void run(Game aGame, Random rand) {
-		do {
-			
-			aGame.roll();
-			
-			if (rand.nextInt(9) == 7) {
-				notAWinner = aGame.wrongAnswer();
-			} else {
-				notAWinner = aGame.wasCorrectlyAnswered();
-			}
-			
-		} while (notAWinner);
+		do {			
+			aGame.roll();		
+		} while (!aGame.isAWinner());
 	}
 	
 	public static Game initialize(Random rand) {
-		Game aGame = new Game(new SingleDice(rand));
-		aGame.add("Chet");
-		aGame.add("Pat");
-		aGame.add("Sue");
-	/*	aGame.add("Chet2");
-		aGame.add("Pat2");
-		aGame.add("Sue2");
-		*/
+		Game aGame = new Game(rand, new SingleDice());
+		aGame.addPlayer("Chet");
+		aGame.addPlayer("Pat");
+		aGame.addPlayer("Sue");
 		return aGame;
 	}
+	
+
 }
