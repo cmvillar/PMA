dab78437404974856dac7ac258764f24a098b1f4
cmvillar <cesarvillarcar@yahoo.es>
2013-04-20 17:36:14 +0200

    Pongo el Test en Rojo

---
+++ triviaPMA/src/com/adaptionsoft/games/trivia/test/TriviaTest.java
@@ -0,0 +1,16 @@
+package com.adaptionsoft.games.trivia.test;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+
+public class TriviaTest {
+
+	
+	@Test
+	public void outputStream() {
+		Assert.assertFalse(true);
+	}
+
+}
