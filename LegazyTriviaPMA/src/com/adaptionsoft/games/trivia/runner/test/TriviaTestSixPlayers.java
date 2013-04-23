c4047f1a80ce3ce52c2c6ae536d90cbc894654f1
cmvillar <cesarvillarcar@yahoo.es>
2013-04-23 23:24:39 +0200

    Se añade el test para seis jugadores

---
+++ LegazyTriviaPMA/src/com/adaptionsoft/games/trivia/runner/test/TriviaTestSixPlayers.java
@@ -0,0 +1,33 @@
+package com.adaptionsoft.games.trivia.runner.test;
+import java.io.PrintStream;
+import java.util.Random;
+
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.adaptionsoft.games.trivia.dice.SingleDice;
+import com.adaptionsoft.games.trivia.outputdevice.ConsoleDevice;
+import com.adaptionsoft.games.trivia.runner.GameRunner;
+import com.adaptionsoft.games.uglytrivia.Game;
+
+public class TriviaTestSixPlayers extends AbstractTriviaTest{
+
+	@Before
+	public void setUp(){
+		checker = new Checker();
+	}
+	
+	@Test
+	public void outputStreamSixPlayers() {
+		System.setOut(new PrintStream(checker));
+		Random rand = new Random(0L);
+		Game game = GameRunner.initializeSixPlayers(rand, new SingleDice(), new ConsoleDevice());
+		GameRunner.run(game, rand);
+		Assert.assertEquals(590124755L, checker.checksum.getValue());
+		
+	}
+
+}
