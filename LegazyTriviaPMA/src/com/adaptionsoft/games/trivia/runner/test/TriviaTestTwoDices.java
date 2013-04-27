9ac6ca8834352dd0a332b11e89719fbc57934458
cmvillar <cesarvillarcar@yahoo.es>
2013-04-27 20:23:18 +0200

    Se hace que falle el test para jugar con dos dados

---
+++ LegazyTriviaPMA/src/com/adaptionsoft/games/trivia/runner/test/TriviaTestTwoDices.java
@@ -0,0 +1,32 @@
+package com.adaptionsoft.games.trivia.runner.test;
+import java.io.PrintStream;
+import java.util.Random;
+
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.adaptionsoft.games.trivia.dice.TwoDices;
+import com.adaptionsoft.games.trivia.outputdevice.ConsoleDevice;
+import com.adaptionsoft.games.trivia.runner.GameRunner;
+import com.adaptionsoft.games.uglytrivia.Game;
+
+public class TriviaTestTwoDices extends AbstractTriviaTest{
+
+	@Before
+	public void setUp(){
+		checker = new Checker();
+	}
+	
+	@Test
+	public void outputStreamTenSidedDice() {
+		System.setOut(new PrintStream(checker));
+		Random rand = new Random(0L);
+		Game game = GameRunner.initialize(rand, new TwoDices(), new ConsoleDevice());
+		GameRunner.run(game, rand);
+		Assert.assertEquals(1830819975L, checker.checksum.getValue());
+	}
+
+}
