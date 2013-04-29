4c55653cef3824ad3d48cc492d6087ae5be3e7fb
cmvillar <cesarvillarcar@yahoo.es>
2013-04-29 22:48:35 +0200

    Refactorización: simplificar en una únnica clase al creación del mensaje

--- LegazyTriviaPMA/src/com/adaptionsoft/games/trivia/messages/MessageBuilder.java
+++ LegazyTriviaPMA/src/com/adaptionsoft/games/trivia/messages/MessageBuilder.java
@@ -1,111 +1,73 @@
 package com.adaptionsoft.games.trivia.messages;
 
+import java.io.IOException;
 import java.text.MessageFormat;
+import java.util.Properties;
 
 import com.adaptionsoft.games.trivia.outputdevice.OutputDevice;
 
 public class MessageBuilder {
 	private OutputDevice outputDevice;
+	private static Properties properties = new Properties();
 	
 	public MessageBuilder(OutputDevice outputDevice){
 		this.outputDevice=outputDevice;
+		try {
+			properties.load(MessageBuilder.class.getResourceAsStream("mensajes_param.properties"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private String getValue(String key){
+		return properties.getProperty(key);
 	}
 	
 	public void showRollDice(int roll) {
-		outputDevice.showMessage(MessageFormat.format(MessageManager.getInstance().getValue("player.roll"),roll));
+		outputDevice.showMessage(MessageFormat.format(getValue("player.roll"),roll));
 	}
 	
 	public void showCurrentPlayer(String name) {
-		outputDevice.showMessage(MessageFormat.format( MessageManager.getInstance().getValue("player.name"),name));
+		outputDevice.showMessage(MessageFormat.format(getValue("player.name"),name));
 	}
 
 
 	public void showPlayerIsNotGettingOutOfPenaltyBox(String name) {
-		outputDevice.showMessage(MessageFormat.format(MessageManager.getInstance().getValue("player.is.not.getting.out.penalty"),name));
+		outputDevice.showMessage(MessageFormat.format(getValue("player.is.not.getting.out.penalty"),name));
 	}
 
 
 	public void showPlayerIsGettingOutOfPenaltyBox(String name) {
-		outputDevice.showMessage(MessageFormat.format(MessageManager.getInstance().getValue("player.is.getting.out.penalty"),name));
+		outputDevice.showMessage(MessageFormat.format(getValue("player.is.getting.out.penalty"),name));
 	}
 
 
 	public void showCurrentLocationAndCategory(String name, int place, String category) {
-		outputDevice.showMessage(MessageFormat.format(MessageManager.getInstance().getValue("player.new.location"),new Object[]{name,"'",place}));
-		outputDevice.showMessage(MessageFormat.format(MessageManager.getInstance().getValue("category"),category));
+		outputDevice.showMessage(MessageFormat.format(getValue("player.new.location"),new Object[]{name,"'",place}));
+		outputDevice.showMessage(MessageFormat.format(getValue("category"),category));
 	}
 	
 	public void showPlayerSentToPenalty(String name) {
-		outputDevice.showMessage(MessageFormat.format(MessageManager.getInstance().getValue("player.sent.penalty"),name));
+		outputDevice.showMessage(MessageFormat.format(getValue("player.sent.penalty"),name));
 	}
 
 	public void showQuestionWasIncorrectlyAnswerd() {
-		outputDevice.showMessage(MessageManager.getInstance().getValue("question.incorrenctly.answered"));
+		outputDevice.showMessage(getValue("question.incorrenctly.answered"));
 	}
 	
 	public void showPlayerGoldenCoins(String name, int purses) {
-		outputDevice.showMessage(MessageFormat.format(MessageManager.getInstance().getValue("golden.coins"),name,purses));
+		outputDevice.showMessage(MessageFormat.format(getValue("golden.coins"),name,purses));
 	}
 
 	public void showAnswerWasCorrect() {
-		outputDevice.showMessage(MessageManager.getInstance().getValue("answer.correct"));
+		outputDevice.showMessage(getValue("answer.correct"));
 	}
 
 	public void showNumberOfPlayers(int numberOfPlayers) {
-		outputDevice.showMessage(MessageFormat.format(MessageManager.getInstance().getValue("number.of.players") ,numberOfPlayers));
+		outputDevice.showMessage(MessageFormat.format(getValue("number.of.players") ,numberOfPlayers));
 	}
 	
 	public void showPlayerNameAdded(String playerName) {
-		outputDevice.showMessage(MessageFormat.format(MessageManager.getInstance().getValue("user.added"),playerName));
+		outputDevice.showMessage(MessageFormat.format(getValue("user.added"),playerName));
 	}
-	
-/*	public void showRollDice(int roll) {
-		outputDevice.showMessage(MessageManager.getInstance().getValue("player.roll")+" "+ roll);
-	}
-	
-	public void showCurrentPlayer(String name) {
-		outputDevice.showMessage(name + " " + MessageManager.getInstance().getValue("player.name"));
-	}
-
-
-	public void showPlayerIsNotGettingOutOfPenaltyBox(String name) {
-		outputDevice.showMessage(name + " " + MessageManager.getInstance().getValue("player.is.not.getting.out.penalty"));
-	}
-
-
-	public void showPlayerIsGettingOutOfPenaltyBox(String name) {
-		outputDevice.showMessage(name + " " + MessageManager.getInstance().getValue("player.is.getting.out.penalty"));
-	}
-
-
-	public void showCurrentLocationAndCategory(String name, int place, String category) {
-		outputDevice.showMessage(name + MessageManager.getInstance().getValue("player.new.location")+" "+place);
-		outputDevice.showMessage(MessageManager.getInstance().getValue("category")+" "+category);
-	}
-	
-	public void showPlayerSentToPenalty(String name) {
-		outputDevice.showMessage(name+ " " + MessageManager.getInstance().getValue("player.sent.penalty"));
-	}
-
-	public void showQuestionWasIncorrectlyAnswerd() {
-		outputDevice.showMessage(MessageManager.getInstance().getValue("question.incorrenctly.answered"));
-	}
-	
-	public void showPlayerGoldenCoins(String name, int purses) {
-		outputDevice.showMessage(name + " " + MessageManager.getInstance().getValue("golden.coins.has")
-				+ " " + purses + " " + MessageManager.getInstance().getValue("golden.coins.goldcoins"));
-	}
-
-	public void showAnswerWasCorrect() {
-		outputDevice.showMessage(MessageManager.getInstance().getValue("answer.correct"));
-	}
-
-	public void showNumberOfPlayers(int numberOfPlayers) {
-		outputDevice.showMessage(MessageManager.getInstance().getValue("number.of.players") + " " + numberOfPlayers);
-	}
-	
-	public void showPlayerNameAdded(String playerName) {
-		outputDevice.showMessage(playerName+ " " + MessageManager.getInstance().getValue("user.added"));
-	}
-	*/
 }
