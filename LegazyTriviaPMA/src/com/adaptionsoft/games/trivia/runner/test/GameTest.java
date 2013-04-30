4cde24958f809ea66d4eef7251b2b428bcf6b162
cmvillar <cesarvillarcar@yahoo.es>
2013-04-30 23:49:10 +0200

    Falla TEST: Se añade la clase de Test para Game

---
+++ LegazyTriviaPMA/src/com/adaptionsoft/games/trivia/runner/test/GameTest.java
@@ -0,0 +1,13 @@
+package com.adaptionsoft.games.trivia.runner.test;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+
+public class GameTest {
+	@Test
+	public void addPlayer() {
+		Assert.fail();
+	}
+}
